238c0717490ad35846f7661ab9edd7dcdf5af6ac
M. kautsar F <18219039@std.stei.itb.ac.id>
2023-07-25 12:03:51 +0700

    fix haptic

--- Assets/Scripts/HapticInteractable.cs
+++ Assets/Scripts/HapticInteractable.cs
@@ -3,31 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class HapticInteractable : MonoBehaviour
+[System.Serializable]
+
+public class Haptic
 {
+
     [Range(0, 1)]
     public float intensity;
     public float duration;
+
+    public void TriggerHaptic(BaseInteractionEventArgs eventArgs)
+        {
+            if (eventArgs.interactorObject is XRBaseControllerInteractor controllerInteractor)
+            {
+                TriggerHaptic(controllerInteractor.xrController);
+            }
+        }
+
+        public void TriggerHaptic(XRBaseController controller)
+        {
+            if (intensity > 0)
+            {
+                controller.SendHapticImpulse(intensity, duration);
+            }
+        }
+}
+
+public class HapticInteractable : MonoBehaviour
+{
+    public Haptic hapticOnActivated;
+    public Haptic hapticSelectEntered;
+    public Haptic hapticHoverEntered;
+    public Haptic hapticHoverExited;
     // Start is called before the first frame update
     void Start()
     {
         XRBaseInteractable interactable = GetComponent<XRBaseInteractable>();
-        interactable.activated.AddListener(TriggerHaptic);
+        interactable.activated.AddListener(hapticOnActivated.TriggerHaptic);
+        interactable.selectEntered.AddListener(hapticSelectEntered.TriggerHaptic);
+        interactable.hoverEntered.AddListener(hapticHoverEntered.TriggerHaptic);
+        interactable.hoverExited.AddListener(hapticHoverExited.TriggerHaptic);
     }
 
-    public void TriggerHaptic(BaseInteractionEventArgs eventArgs)
-    {
-        if(eventArgs.interactorObject is XRBaseControllerInteractor controllerInteractor)
-        {
-            TriggerHaptic(controllerInteractor.xrController);
-        }
-    }
-
-    public void TriggerHaptic(XRBaseController controller)
-    {
-        if (intensity >0)
-        {
-            controller.SendHapticImpulse(intensity, duration);
-        }
-    }
+    
 }
